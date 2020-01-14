9843abd91363088e03eeeb9131db4482caeb54e5
Jack Overby <jroverby92@gmail.com>
2020-01-14 21:12:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,25 @@
 # require modules here
+require "yaml"
+require "pp"
 
-def load_library
-  # code goes here
+def load_library(path)
+  YAML.load_file(path)
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, ho)
   # code goes here
+  reche = load_library(path)
+  reche = reche.map{ |k, v| Hash[v[0], v[1]] }
+  heche = 
+  reche[ho]
 end
 
 def get_english_meaning
   # code goes here
 end
+
+data = load_library("emoticons.yml")
+remapped = data.map{ |k, v| Hash[v[0], v[1]] }
+pp remapped
+
+puts get_japanese_emoticon("emoticons.yml", "O:)")
